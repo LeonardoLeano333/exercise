86921c508df8492b0cc4459c6c54407d73e8081d
Leo <leodrl9@gmail.com>
2021-12-26 11:49:24 -0300

    add check open close braces

--- checklist.md
+++ checklist.md
@@ -1,7 +1,7 @@
 1) okay a
 2) okay b
-3) c
-4) d
+3) okay c
+4) okay d but without unit test
 5) okay e not implemented code have to
 6) okay f
 7) g
--- exercise_d.py
+++ exercise_d.py
@@ -1,3 +1,30 @@
-# exercise 3
-import re
+# 4) Write an efficient method that tells us whether or not an input string brackets ("{", "}",
+# "(", ")", "[", "]") opened and closed properly. Example: “{[]}” => true, “{(])}” => false,
+# “{([)]}” => false
 
+def check_close_braces(text, open_close_rules={"{":"}","[":"]","(":")"}):
+    open_rules = open_close_rules.keys()
+    close_rules = open_close_rules.values()
+    is_open=False
+    for char in text:
+        # check if s is a open char
+        if char in open_rules:
+            open_rule = char # get current open_rule
+            close_rule = open_close_rules[open_rule] # get the current close rule
+            is_open = True
+        elif is_open and char == close_rule:
+            is_open = False
+        elif is_open and char != close_rule and char in close_rules:
+            return False
+    if is_open:
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    asd = "{}{" # false
+    qwe = "{[]}" # true
+    zxc = "{([)]}" # false
+    print( check_close_braces(asd))
+    print( check_close_braces(qwe))
+    print( check_close_braces(zxc))
